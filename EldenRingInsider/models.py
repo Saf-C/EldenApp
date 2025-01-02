4318d5deb9ca526c1cbe34c3baab070e608289d4
Safwan <safwanchoudhury034@gmail.com>
2025-01-02 19:09:10 +0000

    added descriptions when hovering items which are now grouped by item type

--- EldenRingInsider/models.py
+++ EldenRingInsider/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100)
     item_type = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
--- EldenRingInsider/views.py
+++ EldenRingInsider/views.py
@@ -7,13 +7,27 @@ from django.shortcuts import render
 # EldenRingInsider/views.py
 from django.shortcuts import render
 from .models import Item
+from itertools import groupby
+from operator import attrgetter
+
 
 def item_list(request):
-    # Fetch all items from the database
-    items = Item.objects.all()
+    items = Item.objects.all().order_by('item_type')
 
-    # Pass all items to the template
-    return render(request, 'item_list.html', {'items': items})
+    for item in items:
+        print(f"Item: {item.name}, Type: {item.item_type}, Description: {item.description}")
+
+    grouped_items = {k: list(v) for k, v in groupby(items, attrgetter('item_type'))}
+
+    # Debug: Print the structure of grouped_items
+    for item_type, items_list in grouped_items.items():
+        print(f"Item Type: {item_type}")
+        for item in items_list:
+            print(f"  - {item.name}: {item.description[:200]}...")  # Print first 200 chars of description
+
+    return render(request, 'item_list.html', {'grouped_items': grouped_items})
+
+
 
 
 
