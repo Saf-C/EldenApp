5504f31705cbd0bdddedc88c70826c1f6a2afc8b
Safwan <safwanchoudhury034@gmail.com>
2025-01-16 18:24:48 +0000

    tried and attempted to change views and model as well as the grouping of the items, its successfull but not quite what is desired

--- EldenRingInsider/models.py
+++ EldenRingInsider/models.py
@@ -6,11 +6,13 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    item_type = models.CharField(max_length=100)
     description = models.TextField()
+    image_url = models.CharField(max_length=255, default='/static/placeholder.jpg')
+    type = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
 
 
 
--- EldenRingInsider/views.py
+++ EldenRingInsider/views.py
@@ -8,24 +8,26 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Item
 from itertools import groupby
-from operator import attrgetter
-
 
 def item_list(request):
-    items = Item.objects.all().order_by('item_type')
+    # Fetch all items from the database
+    items = Item.objects.all()
 
+    # Group items by their type
+    grouped_items = {}
     for item in items:
-        print(f"Item: {item.name}, Type: {item.item_type}, Description: {item.description}")
+        if item.type not in grouped_items:
+            grouped_items[item.type] = []
+        grouped_items[item.type].append(item)
 
-    grouped_items = {k: list(v) for k, v in groupby(items, attrgetter('item_type'))}
-
-    # Debug: Print the structure of grouped_items
-    for item_type, items_list in grouped_items.items():
-        print(f"Item Type: {item_type}")
-        for item in items_list:
-            print(f"  - {item.name}: {item.description[:200]}...")  # Print first 200 chars of description
-
+    # Pass the grouped items to the template
     return render(request, 'item_list.html', {'grouped_items': grouped_items})
+
+
+
+
+
+
 
 
 
