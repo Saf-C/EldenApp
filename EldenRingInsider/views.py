639dd1653a7e674882d7e646dbe55d88f69a7724
Safwan <safwanchoudhury034@gmail.com>
2025-05-15 13:29:31 +0100

    added pagination and cleaned up views.py

--- EldenRingInsider/views.py
+++ EldenRingInsider/views.py
@@ -1,36 +1,13 @@
-
-
-# Create your views here.
-
-
-
-# EldenRingInsider/views.py
-from .models import Item, ItemType
-from django.db.models import Q  # Import Q for complex queries
-from itertools import groupby
 from django.shortcuts import render, get_object_or_404
-from .models import Build
 from django.http import JsonResponse
 from django.core.paginator import Paginator
 from collections import OrderedDict, defaultdict
+from .models import Item, Build
+from django.db.models import Q
 
 def item_list(request):
-
-    # Get the search query from the request
     query = request.GET.get('q', '')
-
-    # Filter based on query or get all items
-    if query:
-        items = Item.objects.filter(
-            Q(name__icontains=query) |
-            Q(description__icontains=query) |
-            Q(type__icontains=query)
-        )
-    else:
-        items = Item.objects.all().order_by('type', 'name')
-
-
-    ITEM_TYPE_ORDER = [
+    item_type_order = [
         'Armor',
         'Weapon',
         'Spell',
@@ -40,48 +17,48 @@ def item_list(request):
         'Other',
     ]
 
-    # Grouping of items
+    if query:
+        items = Item.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(type__icontains=query)
+        ).order_by('type', 'name')
+    else:
+        items = Item.objects.all().order_by('type', 'name')
+
+    paginator = Paginator(items, 50)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # Group only the items on the current page
     temp_grouped = defaultdict(list)
-    for item in items:
+    for item in page_obj.object_list:
         temp_grouped[item.get_type_display()].append(item)
 
-    # Order them according to my list
     grouped_items = OrderedDict()
-    for t in ITEM_TYPE_ORDER:
+    for t in item_type_order:
         if t in temp_grouped:
             grouped_items[t] = temp_grouped[t]
-
-    # Anything that is not in the item type categories
     for t in temp_grouped:
         if t not in grouped_items:
             grouped_items[t] = temp_grouped[t]
 
-
-
     return render(request, 'item_list.html', {
         'grouped_items': dict(grouped_items),
         'query': query,
+        'page_obj': page_obj,
     })
 
-
-
 def item_detail(request, item_id):
-    # Get the specific item or return a 404 if not found
     item = get_object_or_404(Item, id=item_id)
     return render(request, 'item_detail.html', {'item': item})
-
 
 def builds_view(request):
     query = request.GET.get('q', '')
     builds = Build.objects.prefetch_related('equipment_slots__item')
-
     if query:
         builds = builds.filter(name__icontains=query)
-
     return render(request, 'builds.html', {'builds': builds, 'query': query})
-
-
-
 
 def item_json_view(request, item_id):
     item = get_object_or_404(Item, id=item_id)
@@ -90,12 +67,3 @@ def item_json_view(request, item_id):
         'description': item.description,
         'image_url': item.image_url,
     })
-
-
-
-
-
-
-
-
-
