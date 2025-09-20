afe9099fc543700e0dc7c3a989a0cb45f97d0406
Safwan <safwanchoudhury034@gmail.com>
2025-09-20 16:17:10 +0100

    changed static root

--- myproject/settings.py
+++ myproject/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "/app/staticfiles"
 
 STATIC_URL = '/static/'
 
